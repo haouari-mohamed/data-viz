7ac84e39e0e63f9a0a049526b6cf83459a91df1a
haouari-mohamed <mohamedhaouari71@gmail.com>
2025-04-14 16:28:55 +0100

    add the data tables

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material Imports
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+// Application Components
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { PlotlyChartComponent } from './components/plotly-chart/plotly-chart.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    DataTableComponent,
+    PlotlyChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    AppComponent,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { SalesData } from '../../models/data.model';
 
 @Component({
   selector: 'app-data-table',
-  imports: [],
   templateUrl: './data-table.component.html',
-  styleUrl: './data-table.component.scss'
+  styleUrls: ['./data-table.component.scss']
 })
-export class DataTableComponent {
+export class DataTableComponent implements OnInit, AfterViewInit {
+  @Input() set data(value: SalesData[]) {
+    this._data = value;
+    this.dataSource.data = this._data;
+  }
+  get data(): SalesData[] {
+    return this._data;
+  }
+  
+  private _data: SalesData[] = [];
+  
+  displayedColumns: string[] = ['id', 'product', 'category', 'region', 'date', 'units', 'amount'];
+  dataSource = new MatTableDataSource<SalesData>([]);
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+  
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
