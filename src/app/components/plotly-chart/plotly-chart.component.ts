93dee4fe1b13ce9d94fbb8de5e1084c56ec5ca28
haouari-mohamed <mohamedhaouari71@gmail.com>
2025-04-14 16:04:42 +0100

    add the component

---
+++ src/app/components/plotly-chart/plotly-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plotly-chart',
+  imports: [],
+  templateUrl: './plotly-chart.component.html',
+  styleUrl: './plotly-chart.component.scss'
+})
+export class PlotlyChartComponent {
+
+}
